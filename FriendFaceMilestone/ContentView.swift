8002b99eaf50aeae79512086fa7046cefcfe4573
shivgirja <amitsrivastava115@yahoo.com>
2021-12-22 18:55:40 +0530

    Initial Commit

---
+++ FriendFaceMilestone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendFaceMilestone
+//
+//  Created by Amit Shrivastava on 22/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FriendFaceMilestone/FriendFaceMilestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceMilestoneApp.swift
+//  FriendFaceMilestone
+//
+//  Created by Amit Shrivastava on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceMilestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
